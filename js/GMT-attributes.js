639229d6bed713388c9285e24c4e64b6c89a9719
plabesores123 <plabesores@pandoblox.com>
2024-06-27 00:44:16 +0800

    Update GMT-attributes.js

--- js/GMT-attributes.js
+++ js/GMT-attributes.js
@@ -8,10 +8,14 @@ $(document).ready(function() {
     }
 
     var pageName = getPageName();
+    console.log('Page Name:', pageName);  // Debugging: Log the page name
 
     if ($carousel.attr('data-ar') === 'blog' && pageName === 'All resources landing') {
+        console.log('Condition met: data-ar attribute is blog and page name is All resources landing');
+
         var $buttons = $carousel.find('.owl-nav > button');
-        
+        console.log('Buttons found:', $buttons.length);  // Debugging: Log the number of buttons found
+
         if ($buttons.length > 0) {
             // Set attributes for the first button
             $buttons.first().attr({
@@ -19,14 +23,20 @@ $(document).ready(function() {
                 'data-click_type': 'Arrow',
                 'data-click_text': 'Left' // Exchange the value to 'Left'
             });
-            
+            console.log('Attributes set for the first button');
+
             // Set attributes for the last button
             $buttons.last().attr({
                 'data-tag_section': 'Rent Insights',
                 'data-click_type': 'Arrow',
                 'data-click_text': 'Right' // Exchange the value to 'Right'
             });
+            console.log('Attributes set for the last button');
+        } else {
+            console.log('No buttons found inside .owl-nav');
         }
+    } else {
+        console.log('Condition not met: data-ar attribute is not blog or page name is not All resources landing');
     }
 });
 </script>
