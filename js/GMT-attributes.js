16998d806c4604e4b2506f27de71c17a410d5d18
plabesores123 <plabesores@pandoblox.com>
2024-06-27 00:40:44 +0800

    Update GMT-attributes.js

--- js/GMT-attributes.js
+++ js/GMT-attributes.js
@@ -1,7 +1,15 @@
+<script>
 $(document).ready(function() {
     var $carousel = $('.ar-carousel.is-feature');
 
-    if ($carousel.attr('data-ar') === 'blog') {
+    // Function to get the page name
+    function getPageName() {
+        return $('title').text().trim(); // Assuming the page name is the document title
+    }
+
+    var pageName = getPageName();
+
+    if ($carousel.attr('data-ar') === 'blog' && pageName === 'All resources landing') {
         var $buttons = $carousel.find('.owl-nav > button');
         
         if ($buttons.length > 0) {
@@ -9,15 +17,16 @@ $(document).ready(function() {
             $buttons.first().attr({
                 'data-tag_section': 'Rent Insights',
                 'data-click_type': 'Arrow',
-                'data-click_text': 'Left'
+                'data-click_text': 'Left' // Exchange the value to 'Left'
             });
             
             // Set attributes for the last button
             $buttons.last().attr({
                 'data-tag_section': 'Rent Insights',
                 'data-click_type': 'Arrow',
-                'data-click_text': 'Right'
+                'data-click_text': 'Right' // Exchange the value to 'Right'
             });
         }
     }
 });
+</script>
