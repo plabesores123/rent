cf21778ebd9556c20a97e2e5333266db77aa17ae
plabesores123 <plabesores@pandoblox.com>
2024-06-29 07:38:18 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -5,18 +5,26 @@ $(document).ready(function() {
     }
 
     // Function for page 'All resources landing'
-    function handleAllResourcesPage() {
-        
+    function removenbsp() {
         $('#remove-nbsp').html(function(i,h){
             return h.replace(/&nbsp;/g,' ');
         });
     }
-        
-$(".lp-cta").click(function() {
-    $(".get-started-form-slider").css({
-    "display" : "block",
-    "transition" : "transform 0.4s ease-in-out 0s",
-    "transform" : "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)"
-    });
-    $(".get-started-form-slider").fadeTo(300, 1);
+    
+    function productsGetStarted() {
+        $(".lp-cta").click(function() {
+            $(".get-started-form-slider").css({
+            "display" : "block",
+            "transition" : "transform 0.4s ease-in-out 0s",
+            "transform" : "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)"
+            });
+            $(".get-started-form-slider").fadeTo(300, 1);
+        });
+    }
+    function GetStartedClose() {
+        $(".body").click(function() {
+            $(".close-wrapper").click(); 
+        });
+    }
+    
 });
