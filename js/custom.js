97c5a3dfe08df49dc91bf563e277613ff72ecec8
plabesores123 <plabesores@pandoblox.com>
2024-06-29 07:30:10 +0800

    Create custom.js

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$('#remove-nbsp').html(function(i,h){
+    return h.replace(/&nbsp;/g,' ');
+});
+$(".lp-cta").click(function() {
+    $(".get-started-form-slider").css({
+    "display" : "block",
+    "transition" : "transform 0.4s ease-in-out 0s",
+    "transform" : "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)"
+    });
+    $(".get-started-form-slider").fadeTo(300, 1);
+});
