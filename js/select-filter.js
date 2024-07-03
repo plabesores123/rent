cfd27f39231dfcddd8943c55863ff6294b98da5e
plabesores123 <plabesores@pandoblox.com>
2024-07-04 02:10:10 +0800

    Update select-filter.js

--- js/select-filter.js
+++ js/select-filter.js
@@ -2,13 +2,7 @@ $(function() {
     
    // Initialize SelectMenu using $("#Cat-Select-3")
             $("#Cat-Select-3").selectmenu({
-                icons: { button: "custom-button" },
-                position: {
-                        my: "left top",  // Position of the dropdown menu relative to the button
-                        at: "left bottom", // Position of the dropdown menu relative to the button
-                        of: "#Cat-Select-3", // Element to position against
-                        collision: "flip" // Adjusts the position if dropdown overflows window
-                },                    
+                icons: { button: "custom-button" },              
                 open: function(event, ui) {
                     // Check if the clear button already exists
                     if (!$(this).data('clear-button-initialized')) {
