d20b5f09cc2d01e7bac86c4b85eca8d106a6875a
plabesores123 <plabesores@pandoblox.com>
2024-07-10 03:01:24 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
             "transition" : "transform 0.3s ease-in-out .3s",
             "transform" : "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)"
             });
-            $(".get-started-form-slider").fadeIn('fast');
+            $(".get-started-form-slider").show().fadeIn('fast');
         });
     }
 
